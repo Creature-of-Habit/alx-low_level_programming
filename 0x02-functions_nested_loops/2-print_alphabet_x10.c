35a08a47e29fdb6aee030796956c58f20bac01ad
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-06 11:41:15 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabets in lowercase ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		char k;
+
+		for (k = 'a'; k < 'z'; k++)
+		{
+			_putchar(k);
+		}
+		
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if character is in lowercase
+ * @c: The character to be checked
+ *
+ * Return: 1 if  character is lowercase. 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int result;
+	char lower = 0;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower == c)
+		result = 1;
+	}
+	return (result);
+}
