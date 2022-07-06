a6b4b93cca8e6e99471bac1c915f84a9d5d380ef
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-06 13:10:14 -0700

    mmm

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,13 +4,13 @@
  * _abs - computes absolute value of an integer
  * @a: value to be computed
  *
- * Return: absolute value of integer 
+ * Return: absolute value of integer
  */
 
 int _abs(int a)
 {
 	int result;
-	
+
 	if (a < 0)
 	{
 		result = a * -1;
@@ -19,6 +19,6 @@ int _abs(int a)
 	{
 		result = a;
 	}
-	
+
 	return (result);
 }
