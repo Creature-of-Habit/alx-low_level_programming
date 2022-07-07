eb12028bd62e99af3358f9941aca42b52d7d5958
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-07 12:57:38 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ *@c: character to be checked
+ *
+ * Return: 1 if c is uppercase. 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	int result;
+
+	if ((c >= 'A') || (c <= 'Z'))
+		result = 1;
+
+	return (result);
+}
