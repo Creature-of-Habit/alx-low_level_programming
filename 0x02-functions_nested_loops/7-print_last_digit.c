b357e9df822a5d096df3f363d323c878036cd57c
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-06 17:19:59 -0700

    mmn

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,15 @@
 
 int print_last_digit(int c)
 {
-	int last = c % 10;
+	int last;
+	
+	last = (c % 10);
+
+	if (last < 0)
+	{
+		last = (-1 * last);
+	}
+	_putchar(last + '0');
 
 	return (last);
 }
