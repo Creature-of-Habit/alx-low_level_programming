1b7c3db3e9459803140b582aa49bed1743795f71
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-07 16:02:56 -0700

    mmm

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,10 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i == 100)
+			printf("Buzz");
+
+		else if (i % 15 == 0)
 			printf("FizzBuzz ");
 
 		else if (i % 5 == 0)
@@ -24,8 +27,6 @@ int main(void)
 		else
 			printf("%d", i);
 	}
-	
-	putchar('\n');
 
 	return (0);
 }
