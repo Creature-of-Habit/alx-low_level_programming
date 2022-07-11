9b0b44417ea8d3f2408df7c71ae85dc88bf9cf7d
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-11 15:38:16 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,11 +16,11 @@ void print_square(int size)
 	{
 		int i;
 
-		for (i = 0; i < n; i++)
+		for (i = 0; i < size; i++)
 		{
 			int k;
 
-			for (k = 0; k < n; k++)
+			for (k = 0; k < size; k++)
 			{
 				_putchar('#');
 			}
