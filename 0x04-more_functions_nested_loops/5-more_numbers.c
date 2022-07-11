d09549a460b4fa965841843630b7319af2d98c3f
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-11 15:53:28 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,16 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (i = 0; i < 15; i++)
+		int j;
+
+		for (j = 0; j < 15; j++)
 		{
-			if (i > 9)
+			if (j > 9)
 			{
 				_putchar(i / 10 + '0');
 			}
 			_putchar(i % 10 + '0');
-			_putchar('\n');
 		}
+		_putcha('\n')
 	}
 }
