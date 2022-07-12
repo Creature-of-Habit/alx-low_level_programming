10431e244cc8f10a802a75a14d0c273cd78d37dd
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-12 13:00:14 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of its input to 98
+ * @*n: pointer variable that holds the memory adress of the input
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
