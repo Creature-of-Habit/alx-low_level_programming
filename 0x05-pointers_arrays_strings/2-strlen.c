ed40769b49beb2fb3d6c8337f1a104da7455e106
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-12 13:31:24 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer with the adress of string input.
+ *
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (1)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	return (count);
+}
