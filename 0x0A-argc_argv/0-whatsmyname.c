b85161e1002de7ab5bc88b601e9c240d3d43b449
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-25 06:47:54 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the name of the program
+ * @argc: number of input arguments
+ * @argv: input arguments
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
