a76ca6892db226945f52d870b2ab451f03a52620
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-27 10:09:55 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len] || s2[len])
+	while (s1[len] != '\0' || s2[len] != '\0')
 		len++;
 	strcat = malloc(len * sizeof(char));
 
