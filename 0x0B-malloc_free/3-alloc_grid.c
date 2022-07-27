2974aa247eca1440f920da9a8c7c3adcc4d9c536
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-27 12:54:51 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,3 +1,5 @@
+#include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -10,5 +12,31 @@
 
 int **alloc_grid(int width, int height)
 {
+	int **arr, i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(height * sizeof(int *));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
 }
