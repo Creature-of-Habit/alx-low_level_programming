d9d5384e8ca9911429e18763ca90c9fda2bc5a48
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-07-30 07:28:54 -0700

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,17 @@
 
 int *array_range(int min, int max)
 {
-	int *mryspc;
-      	int size, i = 0;
+	int *mryspc, size;
+	int i = 0;
 
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
 	mryspc = malloc(sizeof(*mryspc) * size);
-	
+
 	if (mryspc == NULL)
 		return (NULL);
-	while ( min <= max)
+	while (min <= max)
 	{
 		mryspc[i++] = min++;
 	}
