8e3877ad6e51849464bd0e474283362104dedf03
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-01 16:20:26 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-#define UNUSED  __attribute__((unused))
+#define UNUSED  (__attribute__((unused)))
 
 /**
  * main - Prints the name of the file it was compiled from, followed by a new line.
- * @ac: number of input arguments
- * @av: input arguments
+ * @argc: number of input arguments
+ * @argv: input arguments
  *
  * Return: Always 0 (Sucess)
  */
 
-int main(int ac UNUSED, char *av)
+int main(int argc UNUSED, char *argv[])
 {
-	printf("%s\n", av[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
