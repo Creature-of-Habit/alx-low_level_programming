24750e2ed5c455a7af4b6ed4b28d912f85cda894
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-01 14:52:19 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure type with three elements
+ * @name: char pointer with memory address of dog name
+ * @age: float var with dog age
+ * @owner: char pointer with address of owner name
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
