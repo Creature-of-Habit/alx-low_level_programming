ed8baf24af99e27ffc2c59560f4688faeddb0c39
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-04 14:45:06 -0700

    header file

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * array_iterator - function that executes a function given as a parameter on each element of an array.
+ * array_iterator - function that executes a function given as a parameter
+ * on each element of an array.
  * @array: Given array
  * @size: Size of the array
  * @action: Pointer to the function to be used.
