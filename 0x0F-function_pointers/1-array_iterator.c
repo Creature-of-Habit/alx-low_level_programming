e161885c14f239e4d7d94903b12f575ce94b0534
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-05 00:53:57 -0700

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function that executes given parameter on array elements
+ * @array: given array of int elements
+ * @size: size of the given array
+ * @action: function to be executed on each element
+ *
+ */
+
+
