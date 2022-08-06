1ccf349a562a118c39a37cdd3ada0f9af043c499
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-06 14:59:15 -0700

    header file

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,9 +18,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (cmp == NULL)
-		return;
+		return (NULL);
 	if(array == NULL)
-		return;
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
