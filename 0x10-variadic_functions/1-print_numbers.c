e9e26d0b1343485b15c1c3d8614623eee1671ff7
Creature-of-Habit <charliecharleston1121@gmail.com>
2022-08-08 13:32:32 -0700

    sum of parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdargs.h>
+#include <stdio.h>
+#include <stddlib.h>
+
+/**
+ * print_numbers - prints numbers 
+ * @separator: string that separates the numbers
+ * @n: number of variable arguments passed
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	int i, num_args = n;
+	va_list args;
+
+	if (separator == NULL)
+		return;
+	va_start(args, n);
+
+	while (i < (num_args - 1))
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+		i++;
+	}
+	printf("%d", va_arg(args, int));
+	va_end(args);
+	printf("\n");
+
+}
